3ab4af5d302a79906fcfe76f5c84c76b54a74ff0
Cregg Hancock <creggh@gmail.com>
2017-01-23 17:14:01 -0600

    made one small change to the self line

--- main.py
+++ main.py
@@ -46,8 +46,7 @@ class MainHandler(webapp2.RequestHandler):
 
         content = header + fortune_paragraph + number_paragraph + cookie_again_button
 
-        self.response.write(content)
-
+        self.response.out.write(content)
 
 app = webapp2.WSGIApplication( [
      ('/', MainHandler),
